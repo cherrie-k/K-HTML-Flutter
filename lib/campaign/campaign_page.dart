645873b5aef6106ff767e0bdadd5f128e0d6414b
EunjinWoo <esther0904@naver.com>
2024-08-13 23:14:37 +0900

    [Feat] trash recycling feature done

--- lib/campaign/campaign_page.dart
+++ lib/campaign/campaign_page.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:k_html_flutter/campaign/elements/imageUpload.dart';
-import 'package:k_html_flutter/campaign/widgets/title.dart';
-import 'package:k_html_flutter/components/bottomBar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:k_html_flutter/campaign/widgets/title.dart'; // Appbar가 포함된 패키지
+import 'package:k_html_flutter/components/bottomBar.dart'; // bottomBar가 포함된 패키지
 
 class CampaignPage extends StatefulWidget {
   const CampaignPage({super.key});
@@ -13,51 +15,260 @@ class CampaignPage extends StatefulWidget {
 }
 
 class _CampaignPageState extends State<CampaignPage> {
-  XFile? _image; //이미지를 담을 변수 선언
-  final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
+  XFile? file;
+  String explanation = 'dhjsldk';
+  bool trashClicked = true;
+
+  Future<void> _getRecycleExplanation(XFile img) async {
+    if (trashClicked && img != null) {
+      try {
+        var request = http.MultipartRequest(
+          'POST',
+          Uri.parse('http://localhost:3000/upload'),
+        );
+        // 'image'라는 키로 파일 전송
+        request.files.add(await http.MultipartFile.fromPath('image', img.path));
+
+        var response = await request.send();
+
+        if (response.statusCode == 200) {
+          var responseData = await response.stream.bytesToString();
+
+          // 앞뒤 따옴표 제거 처리
+          responseData = responseData.trim();
+          if (responseData.startsWith('"') && responseData.endsWith('"')) {
+            responseData = responseData.substring(1, responseData.length - 1);
+          }
+
+          print("받은 responseData");
+          print(responseData);  // 터미널에 응답 출력
+          setState(() {
+            explanation = responseData;
+          });
+        } else {
+          print('Failed to upload image. Status code: ${response.statusCode}');
+        }
+      } catch (e) {
+        print('Error occurred while uploading image: $e');
+      }
+    }
+  }
+
+  Future<void> _pickImage() async {
+    ImagePicker().pickImage(source: ImageSource.gallery).then((image) {
+      if (image != null) {
+        setState(() {
+          file = image;
+          explanation = 'loading ...';
+          _getRecycleExplanation(image);
+        });
+      }
+    });
+  }
+
+  void _switchMenu() {
+    setState(() {
+      trashClicked = !trashClicked;
+      file = null;
+    });
+    print(trashClicked);
+  }
+
+  List<TextSpan> _buildTextSpans(String responseData) {
+    List<TextSpan> spans = [];
+
+    // 정규 표현식을 사용하여 '번호. 항목: ' 패턴을 찾아서 굵게 표시
+    RegExp exp = RegExp(r'(\d+\.\s[^:]+:\s)');
+    Iterable<RegExpMatch> matches = exp.allMatches(responseData);
+
+    int lastIndex = 0;
+
+    for (RegExpMatch match in matches) {
+      if (match.start > lastIndex) {
+        spans.addAll(_processTextWithLineBreaks(
+          responseData.substring(lastIndex, match.start),
+          FontWeight.normal,
+        ));
+      }
+
+      spans.addAll(_processTextWithLineBreaks(
+        match.group(0)!,
+        FontWeight.bold,
+      ));
+
+      lastIndex = match.end;
+    }
+
+    if (lastIndex < responseData.length) {
+      spans.addAll(_processTextWithLineBreaks(
+        responseData.substring(lastIndex),
+        FontWeight.normal,
+      ));
+    }
+
+    return spans;
+  }
+
+  List<TextSpan> _processTextWithLineBreaks(String text, FontWeight fontWeight) {
+    List<TextSpan> spans = [];
+    List<String> parts = text.split('\\n');  // '\n'을 split의 기준으로 사용
+
+    for (int i = 0; i < parts.length; i++) {
+      spans.add(TextSpan(text: parts[i], style: TextStyle(fontWeight: fontWeight)));
+      if (i < parts.length - 1) {
+        spans.add(TextSpan(text: '\n'));  // 실제 줄바꿈 추가
+      }
+    }
+
+    return spans;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Appbar(),
-          automaticallyImplyLeading: false,
-          centerTitle: false,
-        ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.symmetric(horizontal: 33, vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/cT.svg',
-                        height: 24,
-                      ),
-                      SizedBox(width: 20,),
-                      SvgPicture.asset(
-                        'assets/icons/uC.svg',
-                        height: 21,
-                      ),
-                    ],
-                  )
-              ),
-              Expanded(
-                child: Container(
-                  color: Color(0xFFFFFFFF),
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF5F5F5),
-                      borderRadius: BorderRadius.circular(12.0),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Appbar(),
+        automaticallyImplyLeading: false,
+        centerTitle: false,
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 33, vertical: 10),
+              child: trashClicked
+                  ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/cT.svg',
+                    height: 24,
+                  ),
+                  SizedBox(width: 20,),
+                  GestureDetector(
+                    onTap: _switchMenu,
+                    child: SvgPicture.asset(
+                      'assets/icons/uC.svg',
+                      height: 21,
                     ),
-                    child: Center(
+                  ),
+                ],
+              )
+                  : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: _switchMenu,
+                    child: SvgPicture.asset(
+                      'assets/icons/uT.svg',
+                      height: 21,
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  SvgPicture.asset(
+                    'assets/icons/cC.svg',
+                    height: 24,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Color(0xFFFFFFFF),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: (file != null)
+                    ? trashClicked
+                    ? Flex(
+                  direction: Axis.vertical,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: GestureDetector(
+                        onTap: _pickImage,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Container(
+                              width: double.infinity,
+                              child: Image.file(
+                                File(file!.path),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 15,),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color(0xFF57CE8F),
+                              Color(0xFF31C02D)
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: SingleChildScrollView(
+                            child: Text.rich(
+                              TextSpan(
+                                children: _buildTextSpans(explanation),
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+                    : Expanded(
+                  child: GestureDetector(
+                    onTap: _pickImage,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Image.file(
+                            File(file!.path),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                    : Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF5F5F5),
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: _pickImage,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
@@ -80,10 +291,11 @@ class _CampaignPageState extends State<CampaignPage> {
                   ),
                 ),
               ),
-              bottomBar(),
-            ],
-          ),
-        )
+            ),
+            bottomBar(),
+          ],
+        ),
+      ),
     );
   }
 }
